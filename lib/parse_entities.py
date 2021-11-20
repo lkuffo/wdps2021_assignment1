1a7e832166f2eb8c1cabec7c35ee32adf2774003
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2021-11-20 15:22:15 +0100

    try

--- lib/parse_entities.py
+++ lib/parse_entities.py
@@ -59,4 +59,6 @@ def spacy_dictionary(entities_list):
 
 def parse_entities(raw_text):
     entities_list = spacy_ner_from_text(raw_text)
-    return spacy_dictionary(entities_list).items()
+    return spacy_dictionary(entities_list)
+    # for items in spacy_dictionary(entities_list).items():
+    #     print(items)
