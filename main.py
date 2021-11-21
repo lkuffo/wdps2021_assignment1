95d2ea358244700b931440ce883255a5a5e32bf6
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2021-11-21 20:00:14 +0100

    Dummy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-import gzip
+import gzip, traceback
 from lib.parse_entities import parse_entities
 from lib.search_entities import search_entities
 from lib.disambiguate_entities import disambiguate_entities
@@ -55,6 +55,7 @@ if __name__ == '__main__':
             #print(final_entities)
             write_result(f, final_entities, page_id)
         except Exception as e:
+            traceback.print_exc()
             print (e)
         i += 1
     f.close()
