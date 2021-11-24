bed55e4a58840f4d23231d70b04da99c7e50055d
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2021-11-24 23:18:16 +0100

    Try

--- lib/disambiguate_entities.py
+++ lib/disambiguate_entities.py
@@ -73,6 +73,7 @@ def checkIfPerson(wikiID):
     entityId = wikiID.replace('>', '').split('/')[-1]
     query = """
         PREFIX wd: <http://www.wikidata.org/entity/>  
+        PREFIX wdt: <http://www.wikidata.org/prop/direct/>
         select ?item where {
             ?item wdt:P31 wd:Q5; 
             VALUES ?item {  wd:""" + entityId + """ }
