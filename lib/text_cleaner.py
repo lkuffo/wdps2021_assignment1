54e6b3445c67a59375168aea3d1102320a69e8a2
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2021-11-25 14:50:02 +0100

    Try

--- lib/text_cleaner.py
+++ lib/text_cleaner.py
@@ -18,24 +18,25 @@ def remove_urls(text):
 def remove_special_characters(text):
     text_filtered = []
     text_splitted = text.split("\n")
-    for segment in text_splitted:
+    for segment in text_splitted: # We filter out segments of text with less than 10 characters
         if len(segment) < 10:
             continue
         text_filtered.append(segment)
     text = "\n".join(text_filtered)
-    text = text.replace("\t", " ") #.replace("\n", " ").replace("\r", " ")
+    text = text.replace("\t", " ")
     text = re.sub(' +', ' ', text)
     return text
 
 def clean_text(raw_text):
-
-    text = remove_urls(raw_text)
-    text = remove_mentions(text)
-    text = remove_hashtags(text)
     try:
-        text = remove_special_characters(text)
-    except Exception as e:
-        print ("Catched exception", e)
-        text = text
-
+        text = remove_urls(raw_text)
+        text = remove_mentions(text)
+        text = remove_hashtags(text)
+        try:
+            text = remove_special_characters(text)
+        except Exception as e:
+            print ("Catched exception", e)
+            text = text
+    except: 
+        text = raw_text
     return text
